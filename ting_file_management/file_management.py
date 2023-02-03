32a14e5379ee3123f686860b4c99d630eee3f175
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-02-03 17:10:38 -0300

    feat: implementa a lógica da função 'txt_importer' que retorna uma mensagem utilizando o stderr caso a extensão não seja txt e levanta um erro caso o arquivo não seja encontrado

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+from pathlib import Path
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if Path(path_file).suffix != '.txt':
+        return sys.stderr.write('Formato inválido')
+    try:
+        with open(path_file, 'r') as file:
+            lista = file.readlines()
+            return [line.strip() for line in lista]
+
+    except FileNotFoundError:
+        return sys.stderr.write(f'Arquivo {path_file} não encontrado\n')
+
+# print(txt_importer("statics/arquivo_teste1.csv"))
