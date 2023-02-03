1635c618c97239aa755ea3cac31113032273444b
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-02-03 20:22:03 -0300

    feat: implementa a lógica da função 'remove' que exclui o primeiro elemento da fila caso ela não esteja vazia

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -2,17 +2,24 @@ from ting_file_management.queue import Queue
 from ting_file_management.file_management import txt_importer
 import sys
 
+teste = Queue()
+# teste.enqueue({
+#     "nome_do_arquivo": "arquivo_teste.txt",
+#     "qtd_linhas": 3,
+#     "linhas_do_arquivo": ['a', 'b', 'c']
+# })
+
 
 def process(path_file, instance):
-    lista = txt_importer(path_file)
+    txt_list = txt_importer(path_file)
     
     for i in instance.queue:
         if i['nome_do_arquivo'] == path_file:
             return None
     new_register = {
         'nome_do_arquivo': path_file,
-        'qtd_linhas': len(lista),
-        'linhas_do_arquivo': lista
+        'qtd_linhas': len(txt_list),
+        'linhas_do_arquivo': txt_list
         }
     instance.enqueue(new_register)
     # return print(new_register) # equivalente a linha abaixo
@@ -20,8 +27,14 @@ def process(path_file, instance):
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    try:
+        file_name = instance.queue[0]['nome_do_arquivo']
+        instance.queue.pop(0)
+        return print(f'Arquivo {file_name} removido com sucesso')
+    except IndexError:
+        return print('Não há elementos')
 
+# print(remove(teste))
 
 def file_metadata(instance, position):
     """Aqui irá sua implementação"""
