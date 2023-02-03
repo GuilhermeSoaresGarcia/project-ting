e9c994ae68bc8ae6e9897f813fbe0483f07e5d31
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-02-03 19:51:10 -0300

    feat: implementa a lógica da função 'process' que tranforma o conteúdo importado pela função 'txt_importer' em um dict e armazenada na queue instanciada pela classe 'Queue'

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,22 @@
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    lista = txt_importer(path_file)
+    
+    for i in instance.queue:
+        if i['nome_do_arquivo'] == path_file:
+            return None
+    new_register = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(lista),
+        'linhas_do_arquivo': lista
+        }
+    instance.enqueue(new_register)
+    # return print(new_register) # equivalente a linha abaixo
+    return sys.stdout.write(str(new_register))
 
 
 def remove(instance):
