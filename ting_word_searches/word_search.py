dc29ea82828c4928c826cce63b0773792d3aaab1
Guilherme Soares Garcia <guilherme.garcia@email.com>
2023-02-06 17:11:56 -0300

    feat: implementada a lógica da função 'exists_word' que verifica a existência de uma palavra em todos os arquivos processados

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,37 @@
+from ting_file_management.queue import Queue
+
+teste = Queue()
+teste.enqueue({
+    "nome_do_arquivo": "arquivo_teste.txt",
+    "qtd_linhas": 3,
+    "linhas_do_arquivo": ['Come abacAte', 'pra fazer', 'cocô bonito abacate']
+})
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = []
+    word_occurrecies = []
+    for queue_entry in instance.queue:
+        # FONTE: https://stackoverflow.com/a/28072982/18172843
+        for index, line in enumerate(queue_entry['linhas_do_arquivo']):
+            if word.upper() in line.upper():
+                word_occurrecies.append({'linha': index + 1})
+    if len(word_occurrecies) > 0:
+        result.append(
+            {
+                "palavra": word,
+                # FONTE: https://stackoverflow.com/a/13207713/18172843
+                "arquivo": ", ".join(
+                    [file['nome_do_arquivo'] for file in instance.queue]
+                    ),
+                "ocorrencias": word_occurrecies
+            }
+        )
+        return result
+    return word_occurrecies
+
+
+# print(exists_word('abacate', teste))
 
 
 def search_by_word(word, instance):
